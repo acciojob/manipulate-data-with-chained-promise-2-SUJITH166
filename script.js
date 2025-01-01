a629c331bcc016a95032612a503b99253de44e56
SUJITH166 <85979919+SUJITH166@users.noreply.github.com>
2025-01-01 21:02:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,24 +10,22 @@ function getNumbers() {
 
 getNumbers()
   .then((numbers) => {
-    // First promise: filter out odd numbers after 1 second
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         const evenNumbers = numbers.filter((num) => num % 2 === 0);
         document.getElementById("output").textContent = evenNumbers.join(", ");
         resolve(evenNumbers);
-      }, 4000); // Delay of 1 second
+      }, 1000); // Delay of 1 second
     });
   })
   .then((evenNumbers) => {
-    // Second promise: multiply even numbers by 2 after 2 seconds
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         const multipliedNumbers = evenNumbers.map((num) => num * 2);
         document.getElementById("output").textContent =
           multipliedNumbers.join(", ");
         resolve(multipliedNumbers);
-      }, 6000); // Delay of 2 seconds
+      }, 2000); // Delay of 2 seconds
     });
   })
   .catch((err) => console.error(err));
